9c554d3d0e492db9d3e2af7291ed56e9d1645184
mohankumaronly <mohankumaronly81@gmail.com>
2026-07-12 23:20:26 +0530

    LinkedList add two numbers

--- leetcode/LinkedListLearning.java
+++ leetcode/LinkedListLearning.java
@@ -1,4 +1,5 @@
 package leetcode;
+import java.util.List;
 import java.util.Scanner;
 
 public class LinkedListLearning {
@@ -6,17 +7,27 @@ public class LinkedListLearning {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] arr = new int[n];
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
-        ListNode head = insert(arr);
-        printLinkedList(head);
 
-        int target = scanner.nextInt();
+        int m = scanner.nextInt();
 
-        removeTargetValue(head, target);
+        int[] arr1 = new int[m];
 
-        printLinkedList(head);
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+
+        ListNode node1 = insert(arr);
+        ListNode node2 = insert(arr1);
+
+        printLinkedList(node1);
+        System.out.println();
+        printLinkedList(node2);
+
+        addTwoNumbers(node1, node2);
         scanner.close();
     }
 
@@ -56,6 +67,41 @@ public class LinkedListLearning {
             }
         }
     }
+
+     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyhead = new ListNode(0);
+        ListNode result = dummyhead;
+
+        ListNode current1 = l1;
+        ListNode current2 = l2;
+
+        int carry = 0;
+
+        while (current1 != null || current2 != null || carry != 0) {
+            int val1 = 0;
+            int val2 = 0;
+
+            if(current1 != null) {
+                val1 = current1.val;
+                current1 = current1.next;
+            }
+
+            if(current2 != null) {
+                val2 = current2.val;
+                current2 = current2.next;
+            }
+
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+
+            result.next = new ListNode(sum % 10);
+            result = result.next;
+        }
+
+        return dummyhead.next;
+     }
 }
 
 
